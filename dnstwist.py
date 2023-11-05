eb4e083c9bd543da2eba7ac728c4649e464766fc
J/ <55988027+jkerai1@users.noreply.github.com>
2023-11-05 14:03:05 +0000

    Update dnstwist.py

--- dnstwist.py
+++ dnstwist.py
@@ -4,14 +4,15 @@ import dnstwist #pip install this
 import datetime
 from pathlib import Path
 
+whitelist =["example.com"]
 domainlist = []
 reason =[]
 
 query = input("Domain to twist? ")
 if query == "":
-    query = "microsoft.com" #FallBack
+    query = "microsoft.com" #FallBack  #whitelist.append(query)
 
-IOC_Columns = ["IndicatorType","IndicatorValue","ExpirationTime","Action","Severity","Title","Description","RecommendedActions","RbacGroups","Category","MitreTechniques","GenerateAlert"] #schema
+IOC_Columns = ["IndicatorType","IndicatorValue","ExpirationTime","Action","Severity","Title","Description","RecommendedActions","RbacGroups","Category","MitreTechniques","GenerateAlert"]
 stamp = datetime.datetime.now().strftime("%x").replace("/","-")
 filename = "DNSTwist " + query + stamp + ".csv"
 
@@ -22,11 +23,12 @@ if os.path.exists(filename)== False:
 
 z = dnstwist.run(domain=query, format = 'csv')
 
-for i in z[1:]:
+for i in z[1:]: #First record is our actual domain
     domainlist.append(i['domain'])
     reason.append(i['fuzzer'])
-            
+    
 with open(filename, 'a',newline='') as file:
     writer = csv.writer(file)
     for i in domainlist:
-        writer.writerow(["DomainName",i,"","Block","","DNSTWIST",reason[domainlist.index(i)],"","","","","FALSE"])#Create MDE BlockList
+        if i.replace(' ','') not in whitelist:
+            writer.writerow(["DomainName",i,"","Block","","DNSTWIST",reason[domainlist.index(i)],"","","","","FALSE"])
