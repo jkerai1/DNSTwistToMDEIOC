fd3d6e4ceaa1f967fbe0f5c7a1903a8752ef5cbe
J/ <55988027+jkerai1@users.noreply.github.com>
2023-11-05 20:40:49 +0000

    Update MDEdnstwist.py

--- MDEdnstwist.py
+++ MDEdnstwist.py
@@ -1,5 +1,5 @@
 import os
-import csv
+import csv                                     #Tool Written by Jkerai1 https://github.com/jkerai1
 import dnstwist #pip install this
 import datetime
 from pathlib import Path
@@ -7,23 +7,22 @@ from pathlib import Path
 whitelist =["example.com"]
 domainlist = []
 reason =[]
-print("Tool Written by Jkerai1 https://github.com/jkerai1\n")
 query = input("Domain to twist? ")
 if query == "":
     query = "microsoft.com" #FallBack
 
-IOC_Columns = ["IndicatorType","IndicatorValue","ExpirationTime","Action","Severity","Title","Description","RecommendedActions","RbacGroups","Category","MitreTechniques","GenerateAlert"]
-stamp = datetime.datetime.now().strftime("%x").replace("/","-")
-filename = "DNSTwist " + query + stamp + ".csv"
+IOC_Columns = ["IndicatorType","IndicatorValue","ExpirationTime","Action","Severity","Title","Description","RecommendedActions","RbacGroups","Category","MitreTechniques","GenerateAlert"] #schema
+stamp = datetime.datetime.now().strftime("%x").replace("/","-") 
+filename = "DNSTwist " + query + stamp + ".csv" 
 
-if os.path.exists(filename)== False:
+if os.path.exists(filename)== False: #create file if doesn't exist
     with open(filename, 'a+',newline='') as file:
         writer = csv.writer(file)
         writer.writerow(IOC_Columns)
 
 z = dnstwist.run(domain=query, format = 'csv') #,tld = 'common_tlds.dict') if you need extra TLDs.  OR ,dictionary = 'english.dict') if you need a wider dictionary
 
-for i in z[1:]: #First record is our actual domain
+for i in z[1:]: #First record is our actual domain so skip it
     domainlist.append(i['domain'])
     reason.append(i['fuzzer'])
     
@@ -34,5 +33,4 @@ with open(filename, 'a',newline='') as file:
             try: #Try Converting the PunyCode (xn--)
                 writer.writerow(["DomainName",i.encode('idna').decode('idna'),"","BlockAndRemediate","","Dnstwist "+ reason[domainlist.index(i)],"Reason for DNSTwist Block: " + reason[domainlist.index(i)] + "\nTool written by jkerai1","","","","","FALSE"])#Create MDE BlockList
             except: #fallback
-                print("FALLBACK: " + i.encode('idna').decode('idna'))
-                #writer.writerow(["DomainName",i,"","DNSTWIST",reason[domainlist.index(i)],"","","","","FALSE"])
+                print("Error Adding Domain,unlikely supported by MDE: " + i.encode('idna').decode('idna')) #writer.writerow(["DomainName",i,"","DNSTWIST",reason[domainlist.index(i)],"","","","","FALSE"])
