ee54549a289b98c3830a38b699b30de24dbaa438
J/ <55988027+jkerai1@users.noreply.github.com>
2023-11-05 15:04:25 +0000

    Create DNSTwistTenantAllowBlockList.ps1

---
+++ DNSTwistTenantAllowBlockList.ps1
@@ -0,0 +1,8 @@
+#Connect Exchange Powershell First https://learn.microsoft.com/en-us/powershell/exchange/connect-to-exchange-online-powershell?view=exchange-ps
+
+$csv = Import-CSV 'YOUR CSV'
+
+foreach($line in $csv){
+    $url = $line.IndicatorValue 
+    New-TenantAllowBlockListItems -ListType Sender -Block -Entries $url -NoExpiration
+}
