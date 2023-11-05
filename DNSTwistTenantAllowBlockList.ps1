88d60b8e7ee80e21b2a53e27d50d2a073171f077
J/ <55988027+jkerai1@users.noreply.github.com>
2023-11-05 15:32:44 +0000

    Update DNSTwistTenantAllowBlockList.ps1

--- DNSTwistTenantAllowBlockList.ps1
+++ DNSTwistTenantAllowBlockList.ps1
@@ -1,8 +1,10 @@
 #Connect Exchange Powershell First https://learn.microsoft.com/en-us/powershell/exchange/connect-to-exchange-online-powershell?view=exchange-ps
-
 $csv = Import-CSV 'YOUR CSV'
-
+#Sender First then URL
 foreach($line in $csv){
     $url = $line.IndicatorValue 
-    New-TenantAllowBlockListItems -ListType Sender -Block -Entries $url -NoExpiration -Notes "DNS Twist Block"
+    New-TenantAllowBlockListItems -ListType Sender -Block -Entries $url -NoExpiration -Notes "DNS Twist Sender Block"
+    $url = $line.IndicatorValue + "/*"
+    #$url.insert(0,"~") #adjust wildcards accordingly see https://learn.microsoft.com/en-us/microsoft-365/security/office-365-security/tenant-allow-block-list-urls-configure?view=o365-worldwide#url-entry-scenarios
+    New-TenantAllowBlockListItems -ListType Url -Block -Entries $url -NoExpiration -Notes "Dnstwist URL Block" 
 }
